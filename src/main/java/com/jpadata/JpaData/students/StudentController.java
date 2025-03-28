7293721fae42db56c483506884d2c15ade4f2ed8
shubhhmalik <131964323+shubhhmalik@users.noreply.github.com>
2025-03-29 02:37:36 +0530

    Update StudentController.java

--- src/main/java/com/jpadata/JpaData/students/StudentController.java
+++ src/main/java/com/jpadata/JpaData/students/StudentController.java
@@ -9,38 +9,6 @@ import java.util.List;
 @RestController
 public class StudentController {
 
-//    @GetMapping("/hello")
-//    @ResponseStatus(HttpStatus.ACCEPTED)
-//    public String sayHello() {
-//        return "hello from my first controller";
-//    }
-//
-//    @PostMapping("/students")
-//    public Student admission(@RequestBody Student student) {
-//        return repository.save(student);  //basically saves the entity we give to it and returns it
-//    }
-
-
-//    @GetMapping("/students")
-//    public List<Student> findAllStuds(){
-//        return repository.findAll();
-//    }
-//
-//    @GetMapping("/students/{student-id}")
-//    public Student findStudentById(@PathVariable("student-id") Integer id){
-//        return repository.findById(id).orElse(new Student());   //we can also mention null here
-//    }
-
-//    @GetMapping("/students/search/{student-name}")
-//    public List<Student> findStudentByName(@PathVariable("student-name") String name){
-//        return repository.findAllByFirstNameContaining(name);
-   // }
-
-//    @DeleteMapping("/student/{st-id}")
-//    @ResponseStatus(HttpStatus.OK)
-//    public Student delete(@PathVariable("st-id") Integer id){
-//        return repository.deleteById(id);
-//    }
 
     //USING DTO AND SERVICE LAYER
     private final StudentService studentService;
